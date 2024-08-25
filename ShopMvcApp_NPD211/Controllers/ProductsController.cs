0540697c370f2dc78940899e25c4b792b6865762
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-08-25 16:18:19 +0300

    Implement delete product logic.

--- ShopMvcApp_NPD211/Controllers/ProductsController.cs
+++ ShopMvcApp_NPD211/Controllers/ProductsController.cs
@@ -13,6 +13,18 @@ namespace ShopMvcApp_NPD211.Controllers
             var products = context.Products.Include(x => x.Category).ToList();
 
             return View(products);
+        }   
+
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound(); // 404
+
+            context.Products.Remove(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
