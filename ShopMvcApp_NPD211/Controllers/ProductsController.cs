03a8d6496a5df08b178beb717ce6ff28eb397092
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-01 14:55:04 +0300

    Using ViewBag for categories

--- ShopMvcApp_NPD211/Controllers/ProductsController.cs
+++ ShopMvcApp_NPD211/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Data;
 using Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace ShopMvcApp_NPD211.Controllers
@@ -16,15 +17,32 @@ namespace ShopMvcApp_NPD211.Controllers
             return View(products);
         }
 
+        private void LoadCategories()
+        {
+            // Transfer data from controller to View
+            // -1- using model (only 1 object)
+            // -2- using ViewData["Key"]
+            // -3- using ViewBag.Key
+
+            ViewBag.Categories = new SelectList(context.Categories.ToList(), "Id", "Name");
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
+            LoadCategories();
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Product model)
         {
+            if (!ModelState.IsValid)
+            {
+                LoadCategories(); 
+                return View(model);
+            }
+
             context.Products.Add(model);
             context.SaveChanges();
 
