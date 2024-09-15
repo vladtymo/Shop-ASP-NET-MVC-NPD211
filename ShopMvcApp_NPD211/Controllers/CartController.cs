cae7c8f1f1340bd228b1240ad380e7e143c89363
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-15 15:59:04 +0300

    Implement add to cart func

--- ShopMvcApp_NPD211/Controllers/CartController.cs
+++ ShopMvcApp_NPD211/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ShopMvcApp_NPD211.Extensions;
 using ShopMvcApp_NPD211.Models;
 
 namespace ShopMvcApp_NPD211.Controllers
@@ -13,22 +14,31 @@ namespace ShopMvcApp_NPD211.Controllers
 
         public IActionResult Index()
         {
-            //var products = context.Products.Include(x => x.Category).ToList();
-            return View(/*mapper.Map<IEnumerable<ProductModel>>(products)*/);
+            var ids = HttpContext.Session.Get<List<int>>("cartItems") ?? [];
+
+            var products = context.Products.Where(x => ids.Contains(x.Id)).ToList();
+            return View(mapper.Map<IEnumerable<ProductModel>>(products));
         }
 
-        public IActionResult Add()
+        public IActionResult Add(int id)
         {
-            return View();
+            var ids = HttpContext.Session.Get<List<int>>("cartItems") ?? [];
+            ids.Add(id);
+
+            HttpContext.Session.Set("cartItems", ids);
+
+            return RedirectToAction("Index", "Home");
         }
 
-        public IActionResult Remove()
+        public IActionResult Remove(int id)
         {
+            // TODO
             return View();
         }
 
         public IActionResult Clear()
         {
+            // TODO
             return View();
         }
     }
--- ShopMvcApp_NPD211/Program.cs
+++ ShopMvcApp_NPD211/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddDbContext<ShopMvcDbContext>(options =>
 
 builder.Services.AddAutoMapper(typeof(AppProfile));
 
+builder.Services.AddDistributedMemoryCache();
+
+// -------- sessions
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +41,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
